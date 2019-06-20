e098812bbde963bbe076b95adaff5008f7076d20
gereksizvevo <48129300+gereksizvevo@users.noreply.github.com>
2019-06-20 23:08:55 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="tr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" type="image/png" href="favicon.png"/>
+    <title>Resim Yükle - Resim Upload, Resim Paylaþ, Hýzlý Resim</title>
+	<meta name="description" content="Resim Yükle ücretsiz bir resim yükleme servisidir. Resim Yükle ile istediðiniz resmi ücretsiz olarak sunucularýmýzda barýndýrýp paylaþabilirsiniz.">
+	<meta property="og:image" content="http://i.resimyukle.xyz/7KM2.png">
+	<meta property="og:description" content="Resim Yükle ücretsiz bir resim yükleme servisir. Resim Yükle'den istediðiniz resmi sunucularýmýzda barýndýrýp paylaþabilirsiniz.">
+	<meta property="og:site_name" content="Resim Yükle | Resim Upload Sitesi">
+	<meta property="og:title" content="Resim Yükle | Resim Upload Sitesi">
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="http://resimyukle.xyz">
+	<link rel="canonical" href="http://resimyukle.xyz/"/>
+	<style>
+	body{padding:0;margin:0;font-family:Raleway,sans-serif;color:#666;font-weight:500}#header{padding:30px 35px;font-weight:600}a{color:#18bc9c;text-decoration:none;text-shadow:0 0 .1px rgba(0,0,0,.25);transition:.3s all}.beyazarka{background: rgba(255, 255, 255, 0.75);border-radius: 50px;padding: 0 10px;}#logo{float:left}#menu{float:right;font-size:13px}#menu a{color:#ccc;padding-left:10px}#menu a:hover{color:#18bc9c}#clear{clear:both}.bg1, .bg2, .bg3{width:100%;height:100%;position:fixed;z-index:-10000000000000}.bg1{background:url('img/bg1.png');background-size:cover;}.bg2{background:url('img/bg2.png');background-size:cover;}.bg3{
+background:url('img/bg3.png');background-size:cover;}#file-1{width:100%;height:100%;opacity:0;overflow:hidden;position:absolute;z-index:-2;top:0;left:0;}label{transition:.3s all;color:#8e8e8e;background-color:#fff;border:3px solid #dcdcdc;border-radius:33px;padding:0;line-height:60px;padding-left:30px;padding-right:30px;font-size:18px;font-weight:700;font-family:Montserrat,sans-serif;max-width:80%;text-overflow:ellipsis;white-space:nowrap;cursor:pointer;display:inline-block;overflow:hidden;height:60px}label.normal{width:70px;height:70px;background:#18bc9c;border-color:#18bc9c;-webkit-animation:scale-normal 1.3s ease-in-out infinite;animation:scale-normal 1.3s ease-in-out infinite}label:hover{border-color:#18bc9c}#upload{margin-top:calc(50vh - 162px)}#upload h1{font-size:40px;font-weight:500;color:#999}.hidden{display:none}.col-md-3{width:25%;float:left}.col-md-9{width:75%;float:left}.col-md-2{width:25%;float:left}.col-md-10{width:75%;float:left}.col-padding{margin-left:25px}#yuklendi{border:3px solid #f1f1f1;max-width:1100px;margin-left:auto;margin-right:auto;padding-bottom:25px;border-radius:10px;box-sizing:border-box;margin-bottom:30px;background:#fff}#yuklendibaslik{padding-top:16px;padding-bottom:16px;border-bottom:2px solid #f1f1f1;padding-left:25px;margin-bottom:25px}#uploadimg{border:2px solid #e6e6e6;border-radius:5px;padding:5px}#uploadimg img{width:100%;height:auto;margin-bottom:0;padding-bottom:0;border-radius:3px;margin-bottom:-4px}#onizleme{margin-top:40px;margin-left:15px;margin-right:15px}input#uploadinput{font-family:Montserrat,sans-serif;transition:.3s all;display:block;border:0;border-bottom:3px solid #efefef;width:100%;line-height:40px;color:#333;padding:0 15px;box-sizing:border-box;font-size:13px;color:#999}input#uploadinput:focus{border-color:#18bc9c;outline-color:transparent;outline-style:none}#labelup{text-align:right;padding-right:20px;box-sizing:border-box;line-height:43px}#form-group{margin-bottom:15px}#yukleniyor{position:absolute;width:100%;height:100%;top:0;left:0;background:#18bc9c;color:#fff}#herseyuklendi{padding-right:25px}#footer{position:absolute;bottom:10px;width:100%}#footer h1{font-size:22px;font-weight:300;margin:0;margin-bottom:15px}.backlink1{position:absolute;left:30px;font-size:22px;font-weight:300;bottom:15px}.backlink2{position:absolute;right:30px;bottom:0;font-size:22px;font-weight:300;margin-bottom:15px}#geridon{float:right;color:#fff;background:#18bc9c;border-radius:15px;height:44px;line-height:44px;font-size:13px;padding-left:12px;padding-right:12px;cursor:pointer}#gortala{max-width:1100px;margin-left:auto;margin-right:auto;margin-bottom:20px}.loading{position:absolute;left:50%;top:50%;-webkit-transform:translate(-50%,-50%);transform:translate(-50%,-50%)}.loading .circle{width:100px;height:100px;border-radius:50%;background:#f5f5f6;-webkit-animation:scale-normal 1.3s ease-in-out infinite;animation:scale-normal 1.3s ease-in-out infinite}.loading .transparent{opacity:.4;position:absolute;top:0;left:0;-webkit-animation:scale-reverse 1.3s ease-in-out infinite;animation:scale-reverse 1.3s ease-in-out infinite}@media only screen and (max-width: 768px){#upload{margin-top:calc(50vh - 162px)}.backlink1{display:none;}#reklam728{display:none;}.backlink2{display:none;}.col-md-3{display:none}.col-md-9{width:100%}.col-md-2{width:30%}.col-md-10{width:70%}#labelup{text-align:left}}@media only screen and (max-width: 525px){.col-md-3{display:none}.col-md-9{width:100%}.col-md-2{width:100%}.col-md-10{width:100%}#labelup{text-align:left;line-height:35px}#yuklendibaslik{margin-bottom:15px}}@-webkit-keyframes scale-normal{0%{-webkit-transform:scale(0.7);transform:scale(0.7)}50%{-webkit-transform:scale(0.5);transform:scale(0.5)}100%{-webkit-transform:scale(0.7);transform:scale(0.7)}}@keyframes scale-normal{0%{-webkit-transform:scale(0.7);transform:scale(0.7)}50%{-webkit-transform:scale(0.5);transform:scale(0.5)}100%{-webkit-transform:scale(0.7);transform:scale(0.7)}}@-webkit-keyframes scale-reverse{0%{-webkit-transform:scale(0.5);transform:scale(0.5)}50%{-webkit-transform:scale(0.7);transform:scale(0.7)}100%{-webkit-transform:scale(0.5);transform:scale(0.5)}}@keyframes scale-reverse{0%{-webkit-transform:scale(0.5);transform:scale(0.5)}50%{-webkit-transform:scale(0.7);transform:scale(0.7)}100%{-webkit-transform:scale(0.5);transform:scale(0.5)}}
+	</style>
+  </head>
+  <body>
+	<div class="bg1"></div>
+	<div class="bg2"></div>
+	<div class="bg3"></div>
+    <div id="header">
+		<div id="logo"><a href="http://resimyukle.xyz/">Resim Yükle</a></div>
+		<div id="menu">
+			<a href="#">Ýletiþim</a>
+			<a href="#">Hakkýmýzda</a>
+			<a href="#">Blog</a>
+		</div>
+		<div id="clear"></div>
+	</div>
+	<div id="container">
+		<div id="upload">
+			<center>
+				<h1><span class="beyazarka">Resim Yükle</span></h1><br>
+				<div id="uploadyeri">
+					<form action="uploadnew.php" method="post" id="uploadform" name="uploadform" enctype="multipart/form-data">
+					<input type="file" name="resim[]" id="file-1" class="inputfile inputfile-1" data-multiple-caption="{count} dosya seçildi" multiple="">
+					<label for="file-1"><span>Dosya Seç</span></label>
+					</form>
+				</div>
+			</center>
+		<div id="footer">
+		<span class="backlink1"></span>
+		<h1><center><span class="beyazarka">
+			Sitemize Toplamda 149264 Resim Yüklendi
+			</span></center></h1>
+	<span class="backlink2"><a href="http://dizigeek.com/">Yabancý Dizi</a></span>
+	</div>
+		</div>
+		<div id="onizleme"></div>
+	</div>
+	<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+	<script src="jquery.form.min.js"></script>
+	<script src="clipboard.js"></script>
+	<script type="text/javascript">
+		$(function(){
+			$("#file-1").change(function(){
+				$("#onizleme").html('<div id="yukleniyor"><div class="loading"><div class="circle normal"></div><div class="circle transparent"></div></div></div>');
+				$(".bg").addClass("hidden");
+				$("#upload").addClass("hidden");
+				$("#uploadform").ajaxForm({
+					target: '#onizleme'
+				}).submit();
+			});
+		});
+	function geridon() {
+	$("#onizleme").html('');
+    $("#upload").removeClass("hidden");
+}
+	</script>
+	<script>
+	$(document).ready(function() {
+        var yukseklik = $(window).height();
+        var genislik = $(window).width();
+        $("body").mousemove(function(e){
+            var sayfaX = e.pageX - ($(window).width() / 2);
+            var sayfaY = e.pageY - ($(window).height() / 2);
+            $('div.bg1').css("background-position", (25 / genislik * sayfaX)+"px     "+(25 / yukseklik * sayfaY)+"px");
+			$('div.bg2').css("background-position", (-25 / genislik * sayfaX)+"px     "+(-25 / yukseklik * sayfaY)+"px");
+			$('div.bg3').css("background-position", (15 / genislik * sayfaX)+"px     "+(-15 / yukseklik * sayfaY)+"px");
+        });
+	});
+	</script>
+		<script>WebFontConfig={google:{families:["Raleway:300,500,600:latin"]}},function(){var a=document.createElement("script");a.src="https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js",a.type="text/javascript",a.async="true";var b=document.getElementsByTagName("script")[0];b.parentNode.insertBefore(a,b)}();</script>
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-62114382-1', 'auto');
+  ga('send', 'pageview');
+	</script>
+	  </body>
+</html>
